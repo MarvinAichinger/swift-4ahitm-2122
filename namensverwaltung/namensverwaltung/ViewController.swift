ae22e04aa8de2f8c702c0ceb09d7568721fc7062
AHITM01 <AHITM01@leomac01.local>
2021-12-08 23:24:10 +0100

    disable SubmitButton

--- namensverwaltung/namensverwaltung/ViewController.swift
+++ namensverwaltung/namensverwaltung/ViewController.swift
@@ -17,22 +17,33 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        submitButton.isEnabled = false
     }
     
     @IBAction func onChangeVN(_ sender: UITextField) {
         print("vn changed")
+        changeSubmitButtonEnable()
     }
     
     @IBAction func onChangeNN(_ sender: UITextField) {
         print("nn changed")
+        changeSubmitButtonEnable()
     }
     
-    func updateButtonEnable() {
+    func changeSubmitButtonEnable() {
+        if (vornameTextField.text == nil || vornameTextField.text == "" || nachnameTextField.text == nil || nachnameTextField.text == "") {
+            submitButton.isEnabled = false
+        }else {
+            submitButton.isEnabled = true
+        }
     }
     
     @IBAction func submitButtonClicked(_ sender: UIButton) {
         print("Button clicked")
         model.add(vname: vornameTextField.text, nname: nachnameTextField.text)
+        vornameTextField.text = ""
+        nachnameTextField.text = ""
+        changeSubmitButtonEnable()
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
